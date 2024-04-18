76dc85dc7d91f689bac61745f0c8dd4b35410644
Айяр Халилов <super.ayar-04@yandex.ru>
2024-04-18 13:29:05 +0300

    not found logo link to root

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col text-center p-[2vmax] gap-8 items-center h-svh">
-      <img className="h-6" src="/logo.svg" alt="masamadre" />
+      <Link href="/" replace>
+        <img className="h-6" src="/logo.svg" alt="masamadre" />
+      </Link>
       <div className="grow"></div>
       <span className="text-3xl font-bold rotate-90">:)</span>
       <strong className="font-bold text-3xl">
