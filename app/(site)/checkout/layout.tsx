b22488d20b7baf92579c73235fa06b5129b4ee88
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-04-19 08:43:54 +0300

    exact navlink matchers for checkout pages

--- app/(site)/checkout/layout.tsx
+++ app/(site)/checkout/layout.tsx
@@ -7,11 +7,15 @@ export default function CheckoutLayout({ children }: Props) {
   return (
     <div className="px-[2vmax]">
       <div className="flex items-center gap-2 text-xl my-8">
-        <NavLink href="/checkout" activeClassName="underline">
+        <NavLink href="/checkout" activeClassName="underline" exact>
           Доставка
         </NavLink>
         /
-        <NavLink href="/checkout/self-receipt" activeClassName="underline">
+        <NavLink
+          href="/checkout/self-receipt"
+          activeClassName="underline"
+          exact
+        >
           Самовывоз
         </NavLink>
       </div>
