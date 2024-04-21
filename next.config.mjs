03c4f6ddfb19e6449ab53731e9366345c8e14455
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-04-21 15:02:00 +0300

    remove redirects tech works

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  redirects() {
-    return [
-      {
-        source: "/((?!tech-works|logo).*):slug*",
-        destination: "/tech-works",
-        permanent: false,
-      },
-    ];
-  },
+  // redirects() {
+  //   return [
+  //     {
+  //       source: "/((?!tech-works|logo).*):slug*",
+  //       destination: "/tech-works",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
