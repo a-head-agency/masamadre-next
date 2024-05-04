bc34677f1e94a014fa6c181d4ced73ec41f7abd9
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-05-04 13:49:05 +0300

    add qr checkout page

--- app/(site)/checkout/layout.tsx
+++ app/(site)/checkout/layout.tsx
@@ -1,23 +1,33 @@
 import NavLink from "@/components/navlink";
+import { getRests } from "@/data/rests";
+import { getSession } from "@/session";
+import { cookies } from "next/headers";
 import { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {}
 
-export default function CheckoutLayout({ children }: Props) {
+export default async function CheckoutLayout({ children }: Props) {
+  const session = await getSession(cookies());
   return (
-    <div className="px-[2vmax]">
+    <div className="px-[2vmax] min-h-full flex flex-col">
       <div className="flex items-center gap-2 text-xl my-8">
-        {/* <NavLink href="/checkout" activeClassName="underline" exact>
+        {session.tableOrder ? (
+          <span className="underline">Заказ на стол {session.tableOrder.table}</span>
+        ) : (
+          <>
+            {/* <NavLink href="/checkout" activeClassName="underline" exact>
           Доставка
         </NavLink>
         / */}
-        <NavLink
-          href="/checkout/self-receipt"
-          activeClassName="underline"
-          exact
-        >
-          Самовывоз
-        </NavLink>
+            <NavLink
+              href="/checkout/self-receipt"
+              activeClassName="underline"
+              exact
+            >
+              Самовывоз
+            </NavLink>
+          </>
+        )}
       </div>
       {children}
     </div>
--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,23 @@ export async function middleware(request: NextRequest) {
     }
   }
 
+  if (request.nextUrl.pathname.startsWith("/checkout")) {
+    if (request.nextUrl.pathname === "/checkout/qr") {
+      const session = await getSession(cookies());
+      if (!session.tableOrder) {
+        return NextResponse.redirect(new URL("/checkout", request.url));
+      }
+    }
+    if (
+      request.nextUrl.pathname === "/checkout" ||
+      request.nextUrl.pathname === "/checkout/self-receipt"
+    ) {
+      const session = await getSession(cookies());
+      if (session.tableOrder) {
+        return NextResponse.redirect(new URL("/checkout/qr", request.url));
+      }
+    }
+  }
+
   return NextResponse.next();
 }
