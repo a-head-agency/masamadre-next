5a91d099ead08e9595b218ef512e19092798350c
Айяр Халилов <super.ayar-04@yandex.ru>
2024-05-05 19:32:42 +0300

    redirect from checkout page if basket is empty

--- app/(site)/checkout/layout.tsx
+++ app/(site)/checkout/layout.tsx
@@ -1,12 +1,20 @@
 import NavLink from "@/components/navlink";
+import { getBasket } from "@/data/basket";
 import { getSession } from "@/session";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren {}
 
 export default async function CheckoutLayout({ children }: Props) {
   const session = await getSession(cookies());
+  const basket = await getBasket(session)
+
+  if (!basket.list || basket.list.length === 0) {
+    redirect('/order')
+  }
+
   return (
     <div className="px-[2vmax] min-h-full flex flex-col">
       <div className="flex items-center gap-2 text-xl my-8">
