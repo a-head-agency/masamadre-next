c23382aabe615cb7288e0cd5bfb1a92d873006be
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-05-19 17:00:38 +0300

    masamadre logo as link

--- app/not-available-order/page.tsx
+++ app/not-available-order/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function OrderNotAvailbale() {
   return (
-    <div className="min-h-svh flex flex-col items-center select-none p-8">
-      <img className="h-6" src="/logo.svg" alt="masa madre logo" />
+    <div className="min-h-svh flex flex-col items-center select-none p-8 text-center">
+      <Link href="/" replace>
+        <img className="h-6" src="/logo.svg" alt="masa madre logo" />
+      </Link>
       <div className="grow"></div>
       <strong>Заказы онлайн временно не принимаются</strong>
       <div className="grow"></div>
