d624edce608d3f6ce15c91cb0318ebcb51242703
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-05-20 08:34:32 +0300

    fix redirects

--- app/not-available-takeaway/page.tsx
+++ app/not-available-takeaway/page.tsx
@@ -1,10 +1,5 @@
-import { redirect } from "next/navigation";
 
 export default function TakeawaysNotAvailable() {
-  if (process.env.FEATURE_TAKEAWAYS !== "off") {
-    redirect('/');
-  }
-
   return (
     <div className="min-h-svh flex flex-col items-center select-none p-8">
       <img className="h-6" src="/logo.svg" alt="masa madre logo" />
