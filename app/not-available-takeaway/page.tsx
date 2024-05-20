c96574a90d1effeeb8a5621dc130db1062cad376
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-05-20 08:39:20 +0300

    fix styles

--- app/not-available-takeaway/page.tsx
+++ app/not-available-takeaway/page.tsx
@@ -1,10 +1,10 @@
 
 export default function TakeawaysNotAvailable() {
   return (
-    <div className="min-h-svh flex flex-col items-center select-none p-8">
+    <div className="min-h-svh flex flex-col items-center select-none p-8 text-center">
       <img className="h-6" src="/logo.svg" alt="masa madre logo" />
       <div className="grow"></div>
-      <strong>Самовывоз пока не доступен. заказать можно только через qr код в заведении</strong>
+      <strong>Самовывоз пока не доступен. заказать можно только через qr-код в заведении</strong>
       <div className="grow"></div>
     </div>
   );
