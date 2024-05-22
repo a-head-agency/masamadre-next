cd97ff9064906df4423e8c0698bf7f18995fbd7d
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-05-22 14:33:48 +0300

    fix link

--- app/not-available-takeaway/page.tsx
+++ app/not-available-takeaway/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function TakeawaysNotAvailable() {
   return (
     <div className="min-h-svh flex flex-col items-center select-none p-8 text-center">
-      <img className="h-6" src="/logo.svg" alt="masa madre logo" />
+      <Link href="/" replace>
+        <img className="h-6" src="/logo.svg" alt="masa madre logo" />
+      </Link>
       <div className="grow"></div>
       <strong>Самовывоз пока не доступен</strong>
       <div className="grow"></div>
