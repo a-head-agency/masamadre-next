cbfa294713ac44fa835a94ea1f72e9358ba7186d
Ayyar Khalilov <super.ayar-04@yandex.ru>
2024-06-17 19:58:32 +0300

    fix

--- app/(site)/order/[slug]/_widgets/add-to-cart-button-auth.tsx
+++ app/(site)/order/[slug]/_widgets/add-to-cart-button-auth.tsx
@@ -7,9 +7,11 @@ import { DateTime } from "luxon";
 import { useMemo } from "react";
 import { z } from "zod";
 
-
 interface Props {
-  dish: Pick<z.infer<typeof GetOneDishScheme>, 'id' | 'from_hour' | 'to_hour' | 'disabledWhy'>;
+  dish: Pick<
+    z.infer<typeof GetOneDishScheme>,
+    "id" | "from_hour" | "to_hour" | "disabledWhy"
+  >;
   basket_id?: number;
   mods?: number[];
 }
@@ -17,35 +19,36 @@ interface Props {
 export default function AddToCartButtonAuth(props: Props) {
   const basket = useBasket();
 
-  const count = useMemo(
-    () =>
-      basket.data?.list.find((d) => d.dish_id === props.dish.id)?.count || 0,
-    [basket.data, props.dish.id]
+  const item = useMemo(
+    () => basket.data?.list.find((d) => d.id === props.basket_id),
+    [basket.data?.list, props.basket_id]
   );
+
+  const count = item?.count || 0;
+  const id = item?.id || 0;
 
   const disabledMessage = useMemo(() => {
     const dish = props.dish;
     if (dish.disabledWhy) {
       switch (dish.disabledWhy) {
-        case 'time_is_out_of_allowed_range': {
-          const from = DateTime.fromISO(dish.from_hour).toFormat('HH:mm');
-          const to = DateTime.fromISO(dish.to_hour).toFormat('HH:mm');
-          return `С ${from} до ${to}`
+        case "time_is_out_of_allowed_range": {
+          const from = DateTime.fromISO(dish.from_hour).toFormat("HH:mm");
+          const to = DateTime.fromISO(dish.to_hour).toFormat("HH:mm");
+          return `С ${from} до ${to}`;
         }
         default: {
-          return 'Не доступно'
+          return "Не доступно";
         }
       }
     }
-
-  }, [props.dish.disabledWhy])
+  }, [props.dish.disabledWhy]);
 
   return (
     <Button
       isInverted
       onPress={() =>
         basket.addDish({
-          id: props.basket_id,
+          id: id,
           mods: props.mods,
           dish_id: props.dish.id,
           count: count + 1,
@@ -53,7 +56,10 @@ export default function AddToCartButtonAuth(props: Props) {
       }
       isDisabled={basket.isLoading || !!disabledMessage}
     >
-      <span className="text-xs md:text-base lowercase">{disabledMessage || 'добавить в корзину'}</span>
+      <span className="text-xs md:text-base lowercase">
+        {disabledMessage || "добавить в корзину"}
+      </span>
     </Button>
   );
 }
+``;
